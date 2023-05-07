0e725e6ea4175d9ca09c3bf3eeff3a30e121f46f
Arczi4 <urzedowski.artur@gmail.com>
2023-05-07 11:35:02 +0200

    Not working TextRecognition

--- src/components/DisplayPhoto.js
+++ src/components/DisplayPhoto.js
@@ -1,9 +1,28 @@
-import React from "react";
-import { Image, StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import { Image, StyleSheet, View, Button } from "react-native";
 import Swiper from "react-native-swiper";
-import ImageZoom from 'react-native-image-pan-zoom';
+import ImageZoom from "react-native-image-pan-zoom";
+import TextRecognition from "react-native-text-recognition";
 
 const DisplayPhoto = (props) => {
+  const [recognizedText, setRecognizedText] = useState("");
+
+  console.log("1. ", recognizedText);
+
+  async function getRecognizedTextFromImage(imgPath) {
+    
+    const result = await TextRecognition.recognize(imgPath, {
+      visionIgnoreThreshold: 0.5,
+    });
+    console.log("2. ", recognizedText);
+
+    setRecognizedText(result);
+
+    console.log("3. ", recognizedText);
+
+    return recognizedText;
+  }
+
   return (
     <Swiper style={styles.wrapper}>
       {props.route.params.photos.map((photo, index) => {
@@ -14,12 +33,16 @@ const DisplayPhoto = (props) => {
               cropHeight={700}
               imageWidth={390}
               imageHeight={700}
-							minScale={1}
-							maxScale={5}
+              minScale={1}
+              maxScale={5}
               enableCenterFocus={true}
             >
               <Image source={{ uri: photo }} style={styles.image} />
             </ImageZoom>
+            <Button
+              title={"Text recognition"}
+              onPress={getRecognizedTextFromImage(photo)}
+            />
           </View>
         );
       })}

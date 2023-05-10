aa1eea60a099e5c368c6e5a015eae19a3976a6f1
Arczi4 <urzedowski.artur@gmail.com>
2023-05-10 23:17:26 +0200

    Added object recognition

--- src/components/DisplayPhoto.js
+++ src/components/DisplayPhoto.js
@@ -1,27 +1,51 @@
-import React, { useState } from "react";
-import { Image, StyleSheet, View, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Image, StyleSheet, View, Button, Text } from "react-native";
 import Swiper from "react-native-swiper";
 import ImageZoom from "react-native-image-pan-zoom";
 import TextRecognition from "react-native-text-recognition";
+import * as tf from "@tensorflow/tfjs";
+import * as mobilenet from "@tensorflow-models/mobilenet";
 
 const DisplayPhoto = (props) => {
   const [recognizedText, setRecognizedText] = useState("");
+  const [recognizedObject, setRecognizedObject] = useState("");
+  const [imageSource, setImageSource] = useState(null);
 
-  console.log("1. ", recognizedText);
 
-  async function getRecognizedTextFromImage(imgPath) {
-    
-    const result = await TextRecognition.recognize(imgPath, {
-      visionIgnoreThreshold: 0.5,
-    });
-    console.log("2. ", recognizedText);
+  useEffect(() => {
+    const loadModel = async () => {
+      await tf.ready();
+      const model = await mobilenet.load();
+      const tensor = tf.zeros([1, 224, 224, 3]);
+      const result = await model.predict(tensor);
+      console.log(result);
+    };
+    loadModel();
+  }, []);
 
-    setRecognizedText(result);
+  const recognizeTextFromImage = async (image) => {
+    try {
+      const result = await TextRecognition.recognize(image);
+      setRecognizedText(result.text);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    console.log("3. ", recognizedText);
-
-    return recognizedText;
-  }
+  const recognizeObjectFromImage = async (image) => {
+    try {
+      setImageSource(image)
+      const imageTensor = tf.image.decodeJpeg(
+        tf.util.encodeString(imageSource.base64)
+      );
+      const model = await mobilenet.load();
+      const prediction = await model.classify(imageTensor);
+      setRecognizedObject(prediction)
+      console.log(prediction)
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <Swiper style={styles.wrapper}>
@@ -41,8 +65,14 @@ const DisplayPhoto = (props) => {
             </ImageZoom>
             <Button
               title={"Text recognition"}
-              onPress={getRecognizedTextFromImage(photo)}
+              onPress={() => recognizeTextFromImage(photo)}
             />
+            <Button
+              title={"Object detection"}
+              onPress={() => recognizeObjectFromImage(photo)}
+            />
+            <Text>{recognizedText}</Text>
+            <Text>{recognizedObject}</Text>
           </View>
         );
       })}
